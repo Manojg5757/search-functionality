a475406f411394236220b9a80954689b9db5cdaf
manojvsp <gmanojece2021@gmail.com>
2024-06-06 19:34:07 +0530

    feat:changed stylings in search bar

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -6,22 +6,18 @@ const Search = () => {
     // this is used from conext
     const {value,setValue} = useContext(userContext)
   return (
-    <Box sx={{ width: '100%', maxWidth: 200,mx:'auto',mb:'30px' }}>
+    <Box sx={{ width: '100%', maxWidth: 250,mx:'auto',mb:'30px' }}>
     <Input
         type="text"
         value={value}
         onChange={(e) => setValue(e.target.value.toLowerCase())}
         sx={{
             padding: '5px',
-            border: '1px solid #ccc',
             borderRadius: '10px',
             width: '100%',
             height:'50px',
             background:'white',
-            '&:focus': {
-                borderColor: 'blue',
-                boxShadow: '0 0 0 1px blue',
-            },
+            outline:'none'
         }}
     />
 </Box>
